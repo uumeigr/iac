7074baa9e76f55e9ef5cd113a646ca1bd690b73e
uumeigr <jade.android.2011@gmail.com>
2026-02-06 08:27:15 +0800

    Update curl-api.sh

--- curl-api.sh
+++ curl-api.sh
@@ -3,5 +3,17 @@ curl -H "apikey: 替换为PUBLISHABLE_KEY" \
      "https://项目ID.supabase.co/rest/v1/keep_live_ping?select=*"
 
 
+# -H means header
+curl -H "apikey: sb_publishable_SUw0yFl117ArRnW3qKOfbg_bRlTpdOc" \
+     "https://egehivfmeoroomfurmda.supabase.co/rest/v1/public.keep_live_ping?select=*"
+
+
+https://supabase.com/dashboard/project/egehivfmeoroomfurmda/api?resource=keep_alive_ping     
+curl 'https://egehivfmeoroomfurmda.supabase.co/rest/v1/keep_alive_ping?select=*' \
+-H "apikey: sb_publishable_SUw0yFl117ArRnW3qKOfbg_bRlTpdOc" \
+-H "Authorization: Bearer sb_publishable_SUw0yFl117ArRnW3qKOfbg_bRlTpdOc"
+
+
+
 # curl in browser
 https://reqbin.com/curl
