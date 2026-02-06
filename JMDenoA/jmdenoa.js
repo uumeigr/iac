8db8259892df15eb6ebc5b6947104fb4e921f46d
uumeigr <jade.android.2011@gmail.com>
2026-02-06 15:15:23 +0800

    Update jmdenoa.js

--- JMDenoA/jmdenoa.js
+++ JMDenoA/jmdenoa.js
@@ -1,1 +1,11 @@
-import { createClient } from 'npm:@supabase/supabase-js@2'
+//import { createClient } from 'npm:@supabase/supabase-js@2'
+
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello J11!");
+});
+
+Deno.serve(app.fetch);
