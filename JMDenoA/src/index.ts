4b55915058c50957a1242b8a0c44e2fb9acac7e0
uumeigr <jade.android.2011@gmail.com>
2026-02-07 18:24:19 +0800

    Create index.ts

---
+++ JMDenoA/src/index.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@0.91.0/http/server.ts";
+
+const s = serve({ port: 8080 });
+
+console.log("http://localhost:8080/");
+
+for await (const req of s) {
+  req.respond({ body: "Welcome to Deno!\n" });
+}
